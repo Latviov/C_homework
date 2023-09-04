491f842f9d8deb92777ec1b10fb264a953a2c5e1
valtt <valtteri.latvio@metropolia.fi>
2023-09-04 10:52:01 +0300

    5 ex almost

--- ex_5.c
+++ ex_5.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 
 
-int total,num1,num2, number = 0;
+int num;
 
+int read_range(int num1, int num2, int number) {
 
-int read_range(void) {
-    int loops = 0;
-    printf("Enter a number: \n");
-    if (scanf("%d", &num1) != 1) {
-        printf("Invalid input!\n");
+    if (scanf("%d", &number) != 1) {
+        printf("Invalid input!\nPlease enter a number!\n");
+    } else if (number < num1) {
+        printf("Invalid input!\nPlease enter a valid number!\n");
+    } else if (number > num2) {
+        printf("Invalid input!\nPlease enter a valid number!\n");
     }
-    printf("Enter a number: \n");
-    if (scanf("%d", &num2) != 1) {
-        printf("Invalid input!\n");
-    }
+    return number;
 }
 
 int main() {
-    read_range();
+    for (int i = 0; i < 3; i++) {
+        printf("Lets play\nRoll a 6 sided dice and enter the result\n");
+        scanf("%d", &num);
+        int result = read_range(1, 6, num);
+        if (result == 6){
+            printf("I got 6, its a tie!\n");
+        }
+        else printf("I got %d, i win!\n", &result+1);
+
+    }
     return 0;
 }
