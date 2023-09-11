0e69531624aad5a2e905f862ab00fa52ce46dd85
valtt <valtteri.latvio@metropolia.fi>
2023-09-11 09:32:28 +0300

    ex7

--- ex_7.c
+++ ex_7.c
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int number, loops=1;
+int number, loops = 1, money;
 
-bool read_positive(int num){
-    while (loops<3) {
-        printf("Enter a positive number:\n");
-        if (scanf("%d", &number) != 1) {
-            loops++;
-            printf("Invalid input!\nPlease enter a positive number number!\n");
-            scanf("%d", &number);
-        }
-        read_positive(number);
+bool read_positive() {
+    printf("Enter a positive number:\n");
+    if (scanf("%d", &number) != 1) {
+        printf("Invalid input!\nPlease enter a positive number number!\n");
+        scanf("%d", &number);
+        return number;
     }
 }
 
-int main(){
+int main() {
+    for (int i = 0; i < 3; i++) {
+        read_positive();
+        printf("Guess how much money I have?\n");
+        money = number * 2 + 20;
+        printf("You didn't get it right. I have %d \n", money);
 
+    }
 }
